2fe494784c4b810a51c228b0e7eafa2cc37d79b6
Andrey Oliveira <andreyoliveira72005@gmail.com>
2026-01-13 21:39:53 -0300

    feat: adiciona o icone de menu no navbar

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ const nav = document.getElementById("nav");
 window.addEventListener("scroll", function () {
 	nav.classList.toggle("sombra", window.scrollY > 50);
 });
+
+// NAVBAR - MENU
+const menuToggle = document.getElementById("menuToggle");
+const menuMobile = document.getElementById("menuMobile");
+const closeMenu = document.getElementById("closeMenu");
+
+menuToggle.addEventListener("click", () => {
+	menuMobile.classList.add("active");
+});
+
+closeMenu.addEventListener("click", () => {
+	menuMobile.classList.remove("active");
+});
